a6e40f190925355c75dc5d4f57f88e1061f20e6d
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-02 13:56:42 +0530

    add dart hello world program

---
+++ hellodart1.dart
@@ -0,0 +1,11 @@
+// Dart first program : The Hello World program
+// Date: Sun, 02 Oct, 2022
+
+// creatind dart main() method or function
+int main(){
+  
+  print ("Hello, Dart Programming!");
+
+  return 0;
+}
+
