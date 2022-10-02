273ce31fb069f42c8d3983c31ed20f16371d8227
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-02 14:19:01 +0530

    Add comments from line no 14-19

--- hellodart1.dart
+++ hellodart1.dart
@@ -1,3 +1,5 @@
+// hellodart1.dart
+
 // Dart first program : The Hello World program
 // Date: Sun, 02 Oct, 2022
 
@@ -9,3 +11,9 @@ int main(){
   return 0;
 }
 
+/*
+where,
+main() - It is an essential method/function where the program starts its execution. AKA "Entry-point of the program".
+print() - The print() function is a pre-defined function in Dart. The print() function is used to display the specified string/value on the standard output/
+          console screen.
+*/
