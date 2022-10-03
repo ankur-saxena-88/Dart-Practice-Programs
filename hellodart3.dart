fc03289ac3dbcd72a10ddd21d01083c029e752a2
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-03 17:49:58 +0530

    add dart display string code 2

---
+++ hellodart3.dart
@@ -0,0 +1,14 @@
+/*
+Write a dart program to display following string on the console screen using 'main()':
+
+Hello, Ankur Saxena!
+Welcome to the world of Dart Programming!!
+
+Date: Mon, 03 Oct, 2022
+*/
+
+main(){
+
+  print("Hello, Ankur Saxena!");
+  print("Welcome to the world of Dart Programming!!");
+}
