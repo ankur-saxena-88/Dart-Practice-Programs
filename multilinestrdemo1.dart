79acead05a01a6a20f1fb1a1687724f4349f8efe
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-04 10:57:51 +0530

    Edit line no 6-8 & 12-14 of multilinestrdemo.dart file

--- multilinestrdemo1.dart
+++ multilinestrdemo1.dart
@@ -1,2 +1,16 @@
 // Dart multiline string demo program
 // Using Adjacent String Literals
+// Date: Tue, 04 Oct, 2022
+
+void main() {
+   String s1 = 'Hi, Ankur Saxena!'
+     'Welcome to Dart Programming!!'
+     'Let us learn Dart Programming...';
+   print(s1);
+   print('-------'); // Just a horizontal line
+  
+   String s2 = 'Hi, Ankur Saxena!'
+     'Welcome to Dart Programming!!'
+     'Let us learn Dart Programming...';
+   print(s2);
+}
