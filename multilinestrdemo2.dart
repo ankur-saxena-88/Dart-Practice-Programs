283b57fb57cb3c3cbd26a00da3936c37ded67954
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-04 12:34:45 +0530

    Add multilinestrdemo2.dart file

---
+++ multilinestrdemo2.dart
@@ -0,0 +1,25 @@
+// multilinestrdemo2.dart
+
+/*
+Write a dart program to display the following string on the console screen.
+
+Hi, Dart Programming!
+Will this compile?
+Oh yes it will!!
+
+Using triple single quotes
+
+Date: Tue, 04 Oct, 2022
+*/
+
+main(){
+
+  String string1 = '''
+Hi, Dart Programming!
+Will this compile?
+Oh yes it will!!
+''';
+
+  // printing string
+  print (string1);
+}
