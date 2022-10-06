b59df5f58164677abedc45163d756b9a54c06345
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-06 14:02:16 +0530

    Add PrintNumber.dart file

---
+++ PrintNumber.dart
@@ -0,0 +1,16 @@
+// Write a Dart program to print 15 on the console screen.
+// Date: Thu, 06 Oct, 2022
+
+printNum(){
+
+  var num1 = 15;
+
+  // printing num
+  print (num1);
+}
+
+main(){
+
+  // calling function
+  printNum(); // direct call method
+}
