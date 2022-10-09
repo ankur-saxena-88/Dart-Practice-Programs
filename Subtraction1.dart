21498f52b1825ad8fbbdaeaab59893a51b29e1fd
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-09 14:44:43 +0530

    Add Subtraction1.dart file

---
+++ Subtraction1.dart
@@ -0,0 +1,26 @@
+// Write a Dart program to find the subtraction of two numbers.
+// Where, num1 = 65 and num2 = 4
+// Date: Sun, 09 Oct, 2022
+
+// creating dart function
+int findSubtraction(){
+
+  var num1 = 65;
+  var num2 = 4;
+
+  // printing values
+  print ("First number is: $num1");
+  print ("Second number is : $num2");
+
+  // calculate and print subtraction
+  print ("The subtraction of $num1 and $num2 is : ${num1 - num2}");
+
+  return 0;
+}
+
+// creating main method
+main(){
+
+  // calling function
+  print ("${findSubtraction()}");
+}
