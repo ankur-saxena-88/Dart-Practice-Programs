327d197dd31d6e9c8710dc37a67c5ffe654f0367
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-09 14:20:32 +0530

    Add Addition1.dart file

---
+++ Addition1.dart
@@ -0,0 +1,29 @@
+// Write a Dart program to calculate and display the addition of two numbers.
+// Where, num1 = 225 and num2 = 336
+// Date: Sun, 09 Oct, 2022
+
+// creating dart function
+int findAddition(){
+
+  var num1 = 225;
+  var num2 = 336;
+
+  // printing variable values
+  print ("First number is : $num1");
+  print ("Second number is : $num2");
+
+  // calculate sum
+  var sum = num1 + num2;
+
+  // printing sum
+  print ("Sum of $num1 and $num2 is : $sum");
+
+  return 0;
+}
+
+// creating main method or function
+main(){
+
+  // calling function
+  print("${findAddition()}");
+}
