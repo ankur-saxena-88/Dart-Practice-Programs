b51f21b6dd0ba04f1bfac7d4dc3ea194d414e112
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-11 10:59:03 +0530

    Add Dart type-test opp program file

---
+++ TypeTestDemo1.dart
@@ -0,0 +1,14 @@
+// TypeTestDemo1.dart
+
+// Dart type test operator demo program.
+// Date: Tue, 11 Oct, 2022
+
+void main(){
+
+  var name = "Siddharth";
+  var age = 25;
+
+  print (name is String);
+  print (age is int);
+  print (name is! String);
+}
