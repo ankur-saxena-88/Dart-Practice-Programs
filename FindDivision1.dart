4e501494dafbb1420032778a78c43d8641bbeb72
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-12 19:29:56 +0530

    Add Dart division of two numbers program file

---
+++ FindDivision1.dart
@@ -0,0 +1,32 @@
+// FindDivision1.dart
+// Write a Dart program to find the division of two numbers.
+// Where, num1 = 47 and num2 = 4
+// Date: Wed, 12 Oct, 2022
+
+int findDiv(){
+
+  int num1 = 47;
+  int num2 = 4;
+
+  // printing values
+  print ("First number is : $num1");
+  print ("Second number is : $num2");
+
+  // calculating division
+  double division = num1.toDouble() / num2.toDouble();
+
+  // printing division
+  print ("The division of $num1 and $num2 is : $division"); // type-cating or type conversion
+
+  return 0;
+}
+
+int main(){
+
+  // calling function
+  var divResult = findDiv();
+
+  print ("${divResult}");
+
+  return 0;
+}
