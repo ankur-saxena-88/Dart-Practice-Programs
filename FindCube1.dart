e5cd9d67c8fe60a574b4b9151dd7ce92bf9339c1
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-17 17:20:58 +0530

    Add find cube of a number dart file

---
+++ FindCube1.dart
@@ -0,0 +1,16 @@
+// FindCube1.dart
+
+// Write a Dart program to find the cube of a number.
+// Where, num = 6
+// Date: Mon, 17 Oct, 2022
+
+// creating main function
+void main(){
+
+  var num1 = 6;
+
+  print ("Value of num1 is : $num1");
+
+  // calculating and printing cube
+  print ("Cube of $num1 is : ${num1 * (num1 * num1)}");
+}
