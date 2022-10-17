7af9b8bb1c8f2f88776c0df56f4280e78753599f
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-10-17 17:53:14 +0530

    Add print multiplication table of a number program file

---
+++ DisplayTable2.dart
@@ -0,0 +1,32 @@
+// DisplayTable2.dart
+
+// Write a Dart program to display the multiplication table of 15 on the console screen.
+// Date: Mon, 17 Oct, 2022
+
+displayTable1(){
+
+  var num = 15;
+
+  print ("Multiplication table of $num is given below : \n");
+
+  print ("$num x 1 = ${num * 1}");
+  print ("$num x 2 = ${num * 2}");
+  print ("$num x 3 = ${num * 3}");
+  print ("$num x 4 = ${num * 4}");
+  print ("$num x 5 = ${num * 5}");
+  print ("$num x 6 = ${num * 6}");
+  print ("$num x 7 = ${num * 7}");
+  print ("$num x 8 = ${num * 8}");
+  print ("$num x 9 = ${num * 9}");
+  print ("$num x 10 = ${num * 10}");
+  print ("$num x 11 = ${num * 11}");
+  print ("$num x 12 = ${num * 12}");
+}
+
+void main(){
+
+  // calling number
+  var printTable = displayTable1();
+
+  print (printTable);
+}
