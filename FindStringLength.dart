60301ec178a7f0777081b682116b3dd0833555db
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-11-12 22:54:44 +0530

    Add dart code to find the string length

---
+++ FindStringLength.dart
@@ -0,0 +1,26 @@
+// FindStringLength.dart
+
+/*
+Write a Dart program to find the length of a string.
+Test string: Hello, My name is Rover!
+Date: Sat, 12 Nov, 2022
+@author: Ankur Saxena
+*/
+findStrLength(){
+
+  var string = "Hello, My name is Rover!";
+
+  // printing string
+  print ("The string is : $string");
+
+  // printing length
+  print ("Length of the string is : ${string.length}");
+}
+
+main(){
+
+  var displayString = findStrLength();
+
+  // printing result
+  print (displayString);
+}
